7fd077dbfde25e694de1a04e31c7608868867181
swaption2009 <swaption2009@gmail.com>
2016-10-07 21:05:13 -0700

    fetch-and-show start program

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,9 +1,10 @@
 /*
 Instructions:
-(1) Wrap an XHR in a Promise in the get() function below. See: https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest/Using_XMLHttpRequest
-  (a) Resolve on load and reject on error.
-(2) If the XHR resolves, use addSearchHeader to add the search header to the page.
-(3) If the XHR fails, console.log the error and pass 'unknown' to addSearchHeader
+(1) Get the planet data and add the search header.
+(2) Create the first thumbnail with createPlanetThumb(data)
+(3) Handle errors!
+  (a) Pass 'unknown' to the search header.
+  (b) console.log the error.
  */
 
 // Inline configuration for jshint below. Prevents `gulp jshint` from failing with quiz starter code.
@@ -16,58 +17,53 @@ Instructions:
 
   /**
    * Helper function to show the search query.
-   * @param {String} response - The unparsed JSON response from get.
+   * @param {String} query - The search query.
    */
-  function addSearchHeader(response) {
-    try {
-      response = JSON.parse(response).query;  // you'll be moving this line out of here in the next quiz!
-    } catch (e) {
-      // it's 'unknown', so leave it alone
-    }
-    home.innerHTML = '<h2 class="page-title">query: ' + response + '</h2>';
+  function addSearchHeader(query) {
+    home.innerHTML = '<h2 class="page-title">query: ' + query + '</h2>';
   }
 
   /**
-   * XHR wrapped in a promise.
+   * Helper function to create a planet thumbnail.
+   * @param  {Object} data - The raw data describing the planet.
+   */
+  function createPlanetThumb(data) {
+    var pT = document.createElement('planet-thumb');
+    for (var d in data) {
+      pT[d] = data[d];
+    }
+    home.appendChild(pT);
+  }
+
+  /**
+   * XHR wrapped in a promise
    * @param  {String} url - The URL to fetch.
    * @return {Promise}    - A Promise that resolves when the XHR succeeds and fails otherwise.
    */
   function get(url) {
-    // start a Promise
-    return new Promise(function(resolve, reject) {
-      var req = new XMLHttpRequest();
-      req.open('GET', url);
-      req.onload = function() {
-        if (req.status === 200) {
-          // It worked!
-          resolve(req.response);
-        } else {
-          // It failed :(
-          reject(Error(req.statusText));
-        }
-      };
-      req.onerror = function() {
-        // It failed :( Pass a 'Network Error' to reject
-        reject(Error('Network Error'));
-      };
-      req.send();
+    return fetch(url, {
+      method: 'get'
+    });
+  }
+
+  /**
+   * Performs an XHR for a JSON and returns a parsed JSON response.
+   * @param  {String} url - The JSON URL to fetch.
+   * @return {Promise}    - A promise that passes the parsed JSON response.
+   */
+  function getJSON(url) {
+    return get(url).then(function(response) {
+      return response.json();
     });
   }
 
   window.addEventListener('WebComponentsReady', function() {
     home = document.querySelector('section[data-route="home"]');
     /*
-    Uncomment the next line you're ready to start chaining and testing!
-    You'll need to add a .then and a .catch. Pass the response to addSearchHeader on resolve or
-    pass 'unknown' to addSearchHeader if it rejects.
+    Uncomment the next line and start here when you're ready to add the first thumbnail!
+
+    Your code goes here!
      */
-    get('../data/earth-like-results.json')
-    .then(function(response) {
-      addSearchHeader(response);
-    })
-    .catch(function(error) {
-      addSearchHeader('unknown');
-      console.log(error);
-    });
+    // getJSON('../data/earth-like-results.json')
   });
 })(document);
